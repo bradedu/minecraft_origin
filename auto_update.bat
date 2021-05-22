f0c50ab762ed3d5be68fca8a03b606218a5e045c
bradedu <bradedu@gmail.com>
2021-05-23 00:00:10 +0800

    auto back up - 2021/05/23

---
+++ auto_update.bat
@@ -0,0 +1,5 @@
+set mydate=%date:~0,10%
+git add world logs world_nether world_the_end res
+git commit -m "auto back up - %mydate%"
+git push origin master
+
--- auto_update.sh
+++ auto_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-while :
-do
+#while :
+#do
 	date=`date +"%Y%m%d"`
-	git add world logs
+	git add world logs world_nether world_the_end res
 	git commit -m "auto back up - $date"
 	git push origin master
-	sleep 604800
-done
+#	sleep 604800
+#done
 
