4f9e9dfabc5932165674c1dc08bace986853b327
Csongs <danieldu71427@gmail.com>
2021-12-01 19:19:35 +0800

    20211201 - before updating to 1.18

--- auto_update.bat
+++ auto_update.bat
@@ -1,5 +1,5 @@
 set mydate=%date:~0,10%
-git add world logs world_nether world_the_end res
+git add world logs world_nether world_the_end res*
 git commit -m "auto back up - %mydate%"
 git push origin master
 
