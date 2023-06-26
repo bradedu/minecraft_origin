84ac2b203a837ed85ee1cecfbd50cd03f6d904a4
bradedu <bradedu@gmail.com>
2023-06-26 19:25:26 +0800

    backup

--- auto_update.bat
+++ auto_update.bat
@@ -1,5 +1,9 @@
 set mydate=%date:~0,10%
+
+cd /D G:\game\minecraft_server\original_server
+
 git add world logs world_nether world_the_end res*
 git commit -m "auto back up - %mydate%"
 git push origin master
 
+pause
--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-java -Xms4G -Xmx6G  -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -Dusing.aikars.flags=mcflags.emc.gs -jar spigot-1.17.jar
+java -Xms4G -Xmx8G  -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch -Dusing.aikars.flags=mcflags.emc.gs -jar spigot-1.20.jar
 
 pause
 
-:: java -Xms2G -Xmx4G -XX:MaxPermSize=128M -jar spigot-1.17.jar
+:: java -Xms2G -Xmx4G -XX:MaxPermSize=128M -jar spigot-1.18.jar
